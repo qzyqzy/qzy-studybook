a363be9f6d733676dc9b93fd4b59b40f2f7aa3f0
qzyqzy <15757745974@163.com>
2021-11-14 23:08:31 +0800

    反转链表、k个一组翻转链表

--- leetcode/206. 反转链表/index.js
+++ leetcode/206. 反转链表/index.js
@@ -1,1 +1,17 @@
-// todo
+var reverseList = function (head) {
+  // 来吧 先按要求定义上
+  let prev = null;
+  let cur = head;
+  // 当前节点不是尾节点 我就不停止
+  while (cur) {
+    // 临时存储当前节点的下一个节点
+    const next = cur.next;
+    // 反转
+    cur.next = prev;
+    // 都往后移动一位
+    prev = cur;
+    cur = next;
+  }
+  // 最后返回 prev
+  return prev;
+};
