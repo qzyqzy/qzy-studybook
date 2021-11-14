dfa99360c33825d215e17c049294f876e064f225
qzyqzy <15757745974@163.com>
2021-11-14 11:23:18 +0800

    环形链表

---
+++ leetcode/141. 环形链表/index.js
@@ -0,0 +1,21 @@
+var hasCycle = function (head) {
+  // 边界情况处理
+  if (head == null || head.next == null) {
+    return false;
+  }
+  slow = head;
+  fast = head;
+  do {
+    // 快指针到达终点
+    if (fast == null || fast.next == null) {
+      return false;
+    }
+    // 你走一步 我走两步
+    slow = slow.next;
+    fast = fast.next.next;
+
+    // 不追上你我不停
+  } while (slow != fast);
+
+  return true;
+};
