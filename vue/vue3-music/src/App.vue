d17d9803905cb2697e530cb90a019540fe3fba55
qzyqzy <15757745974@163.com>
2021-12-13 23:58:15 +0800

    头部组件创建

--- vue/vue3-music/src/App.vue
+++ vue/vue3-music/src/App.vue
@@ -1,26 +1,18 @@
 <template>
+  <Header />
   <router-view />
 </template>
 
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+  },
+};
+</script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ vue/vue3-music/src/components/Header/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <span class="icon"></span>
+    <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header",
+};
+</script>
+
+<style scoped lang="less">
+.header {
+  height: 44px;
+  color: @color-theme;
+  font-size: 0;
+  text-align: center;
+
+  .icon {
+    display: inline-block;
+    width: 30px;
+    height: 32px;
+    margin-top: 6px;
+    margin-right: 9px;
+    vertical-align: top;
+    background-size: 30px 32px;
+    .bg-image("logo");
+  }
+
+  .text {
+    display: inline-block;
+    font-size: @font-size-large;
+    line-height: 44px;
+    vertical-align: top;
+  }
+
+  .mine {
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    .icon-mine {
+      display: block;
+      padding: 12px;
+      color: @color-theme;
+      font-size: @font-size-large-x;
+    }
+  }
+}
+</style>
